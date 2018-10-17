f95bb977bf427686e545fa098624addf6e36d023
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-10-18 00:51:48 +0800

    Display shopping list items on the Homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Item } from '../../models/items/item.model';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  shoppingList$: Observable<Item[]>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, 
+    private shopping: ShoppingListService) {
+    this.shoppingList$ = this.shopping
+      .getShoppingList()
+      .snapshotChanges()
+      .pipe(
+        map(changes => {
+          return changes.map(change => {
+            const data: Item = change.payload.doc.data() as Item;
+            const id = change.payload.doc.id;
+            return { id, ...data};
+          })
+        })
+      );
   }
 
 }
--- src/services/shopping-list/shopping-list.service.ts
+++ src/services/shopping-list/shopping-list.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
 import { Item } from "../../models/items/item.model";
-import { Observable } from "rxjs";
 
 const SHOPPING_LIST_REF_NAME = 'shopping-list';
 // const SHOPPING_LIST_REF_NAME = 'wendysa-ionic-shopping-list';
@@ -9,15 +8,13 @@ const SHOPPING_LIST_REF_NAME = 'shopping-list';
 @Injectable()
 export class ShoppingListService {
   private shoppingListCollection: AngularFirestoreCollection<Item>;
-  private shoppingList: Observable<Item[]>;
 
   constructor(private db: AngularFirestore) {
     this.shoppingListCollection = this.db.collection<Item>(SHOPPING_LIST_REF_NAME);
-    this.shoppingList = this.shoppingListCollection.valueChanges();
   }
 
-  getShoppingList() {    
-    return this.shoppingList;
+  getShoppingList() : AngularFirestoreCollection<Item> {
+    return this.shoppingListCollection;
   }
 
   addItem(item: Item): any {
