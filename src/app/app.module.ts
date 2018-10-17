f93848ffff77dabde03ed39cebb5dd434e1675e8
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-10-17 14:35:50 +0800

    Added fire base dependencies and enable lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,28 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+import { FIREBASE_CONFIG } from './firebase.credentials';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+// import { HomePage } from '../pages/home/home';           // ** enable Lazy loading **
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp /*,
+    HomePage */                                             // ** enable Lazy loading **
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp /*,
+    HomePage */                                             // ** enable Lazy loading **
   ],
   providers: [
     StatusBar,
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDXFE6zHQUJk4CNw3oRukeeLh3yut3ohMI",
+  authDomain: "wendysa-ionic-shopping-list.firebaseapp.com",
+  databaseURL: "https://wendysa-ionic-shopping-list.firebaseio.com",
+  projectId: "wendysa-ionic-shopping-list",
+  storageBucket: "wendysa-ionic-shopping-list.appspot.com",
+  messagingSenderId: "809942698531"
+};
